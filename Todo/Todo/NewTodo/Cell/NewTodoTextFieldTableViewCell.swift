5a240bb96c5def04a85918fef13a2dd7415ae0e0
kizi <phg980513@naver.com>
2024-02-19 01:28:31 +0900

    [수정] identifier 변경에 따른 BaseTVCell의 자식셀에 이니셜라이저 추가
    - HeegeePark/SeSAC#20

--- Todo/Todo/NewTodo/Cell/NewTodoTextFieldTableViewCell.swift
+++ Todo/Todo/NewTodo/Cell/NewTodoTextFieldTableViewCell.swift
@@ -27,6 +27,10 @@ class NewTodoTextFieldTableViewCell: BaseTableViewCell {
         }
     }
     
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    }
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
