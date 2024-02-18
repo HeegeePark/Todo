6e42985acc099ce8f0a4d1dc48c5d4fc9b8c8b5f
kizi <phg980513@naver.com>
2024-02-19 01:58:49 +0900

    [수정] BaseTVCell 배경색 설정 추가
    - HeegeePark/SeSAC#20

--- Todo/Todo/Base/BaseTableViewCell.swift
+++ Todo/Todo/Base/BaseTableViewCell.swift
@@ -12,6 +12,7 @@ class BaseTableViewCell: UITableViewCell {
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         selectionStyle = .none
+        contentView.backgroundColor = .clear
         configureHierarchy()
         configureLayout()
         configureView()
