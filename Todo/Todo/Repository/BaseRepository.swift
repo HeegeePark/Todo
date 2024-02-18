747c34609ff6ee104963fd2cc49a93cd5c83a4ad
kizi <phg980513@naver.com>
2024-02-19 03:05:20 +0900

    [수정] Realm Repository 구현
    - HeegeePark/SeSAC#20
    - 제네릭 기반 BaseRepository 이용
    - TodoRepository 구현

---
+++ Todo/Todo/Repository/BaseRepository.swift
@@ -0,0 +1,41 @@
+//
+//  BaseRepository.swift
+//  Todo
+//
+//  Created by 박희지 on 2/19/24.
+//
+
+import Foundation
+import RealmSwift
+
+class BaseRepository<T: Object> {
+    typealias Model = T
+    private let realm = try! Realm()
+    
+    func createItem(_ item: T) {
+        do {
+            try realm.write {
+                realm.add(item)
+            }
+        } catch {
+            print(error)
+        }
+    }
+    
+    func fetch() -> Results<Model> {
+        return realm.objects(Model.self)
+    }
+    
+    func updateItem(id: ObjectId, item: Model) {
+    }
+    
+    func deleteItem(object: ObjectBase) {
+        do {
+            try realm.write {
+                realm.delete(object)
+            }
+        } catch {
+            print(error)
+        }
+    }
+}
---
+++ Todo/Todo/Repository/TodoModelRepository.swift
@@ -0,0 +1,12 @@
+//
+//  TodoModelRepository.swift
+//  Todo
+//
+//  Created by 박희지 on 2/19/24.
+//
+
+import Foundation
+import RealmSwift
+
+class TodoModelRepository: BaseRepository<TodoModel> {
+}
